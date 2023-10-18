ad5eb46dfe4e2009f264af8a65ae4cbc67ac2276
DeanYoon <68269605+DeanYoon@users.noreply.github.com>
2023-10-18 15:46:37 +0900

    redirect url modified

--- src/Components/KakaoLogin.tsx
+++ src/Components/KakaoLogin.tsx
@@ -39,7 +39,7 @@ export const KakaoLogin = () => {
   const baseUrl = "https://kauth.kakao.com/oauth/authorize";
   const config = {
     client_id: "fbe17719e0bfefa4b2b6f51dc1bea7a8",
-    redirect_uri: "http://localhost:3000/kakao-login",
+    redirect_uri: "https://open-ai-vercel.vercel.app/kakao-login",
     response_type: "code",
   };
   const params = new URLSearchParams(config).toString();
@@ -75,7 +75,7 @@ export const FinishKakaoLogin = ({ code }: FinishKakaoLoginProps) => {
       client_id: "fbe17719e0bfefa4b2b6f51dc1bea7a8",
       client_secret: "4MTRap8K4TL0tmycZ70bhlSjBPzXwzRY",
       grant_type: "authorization_code",
-      redirect_uri: "http://localhost:3000/kakao-login",
+      redirect_uri: "https://open-ai-vercel.vercel.app/kakao-login",
       code: code as string,
     };
     const params = new URLSearchParams(config).toString();
